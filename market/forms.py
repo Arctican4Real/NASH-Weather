5ff89c6053ec45ceff5b72ae8106a175f6b81a5b
Hr33m <109884770+Hr33m@users.noreply.github.com>
2022-08-04 09:42:36 -0600

    Update forms.py

--- market/forms.py
+++ market/forms.py
@@ -10,9 +10,16 @@ class RegisterForm(FlaskForm):
     confirm_pass = PasswordField(label='Confirm Password', validators=[EqualTo('password'), DataRequired()])
     submit = SubmitField(label='Create Your Account')
 
+    
 class Country(FlaskForm):
     country = StringField(label='Your Country?', validators=[Length(min=2, max=56), DataRequired()])
 
 
 class City(FlaskForm):
     city =  StringField(label='Your City?', validators=[Length(min=2, max=50), DataRequired()])
+
+    
+class LoginForm(FlaskForm):
+    username = StringField(label='User Name:', validators=[DataRequired()])
+    password = PasswordField(label='Password:', validators=[DataRequired()])
+    submit = SubmitField(label='Sign In')
