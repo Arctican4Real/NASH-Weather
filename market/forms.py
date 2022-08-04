2fb1227f9851f1414cc21b6f07af94b7844c16d9
Hr33m <109884770+Hr33m@users.noreply.github.com>
2022-08-04 09:10:26 -0600

    Update forms.py

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='username')
-    email_address = StringField(label='email')
-    password = PasswordField(label='password')
-    confirm_pass = PasswordField(label='confirm_pass')
-    submit = SubmitField(label='submit')
+    username = StringField(label='User Name', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email Address', validators=[Email(), DataRequired()])
+    password = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
+    confirm_pass = PasswordField(label='Confirm Password', validators=[EqualTo('password'), DataRequired()])
+    submit = SubmitField(label='Create Your Account')
