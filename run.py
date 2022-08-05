2455341af6711b76dabd74fa857be537d86c40d3
Neil Shrivastava <neilshri007@gmail.com>
2022-08-05 16:57:01 +0900

    backend debug

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
-from market import app
+
+
+
+from market.routes import app
 
 #Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5000)
+
+
