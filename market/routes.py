b6a5efdd6acca1cdf69f7887eb5a6cec7b77bc71
Neil Shrivastava <neilshri007@gmail.com>
2022-08-05 20:00:58 +0900

    squashed some bugs

--- market/routes.py
+++ market/routes.py
@@ -7,7 +7,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '%f43A9Uo4'
 app.config['MYSQL_DB'] = 'weather_db'
-app.config['SECRET_KEY'] = 'GDtfDCFYryy'
+app.config['SECRET_KEY'] = '422434344re5ttrhttyyty'
 
 mysql = MySQL(app)
 
@@ -22,15 +22,15 @@ def index():
 
 @app.route('/login')
 def login():
-    global username
-    global password
+
     # Output message if something goes wrong...
 
     msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    if request.method == 'GET' and 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
+
     # Check if account exists using MySQL
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute('SELECT * FROM user WHERE username = %s AND password = %s', (username, password))
@@ -52,10 +52,6 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    global username
-    global password
-    global email_address
-
     msg = ''
 
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
@@ -65,34 +61,37 @@ def register():
         username = request.form['username']
         password = request.form['password']
         email_address = request.form['email']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email_address):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email_address:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute("INSERT INTO user (username,city,email_address,country,password) VALUES (%s, %s, %s, %s, %s)", (username, 'Washington', email_address, 'USA', password,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+
+
+
     elif request.method == 'POST':
         # Form is empty... (no POST data)
         msg = 'Please fill out the form!'
 
-
     # Show registration form with message (if any)
-        # Check if account exists using MySQL
+    # Check if account exists using MySQL
+    return render_template('register.html', msg=msg)
 
-
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('SELECT * FROM user WHERE username = %s', (username,))
-    account = cursor.fetchone()
-    # If account exists show error and validation checks
-    if account:
-        msg = 'Account already exists!'
-    elif not re.match(r'[^@]+@[^@]+\.[^@]+'):
-        msg = 'Invalid email address!'
-    elif not re.match(r'[A-Za-z0-9]+', username):
-        msg = 'Username must contain only characters and numbers!'
-    elif not username or not password or not email_address:
-        msg = 'Please fill out the form!'
-    else:
-        # Account doesnt exists and the form data is valid, now insert new account into accounts table
-        cursor.execute("INSERT INTO user (user_id,username,city,email_address,country,password) VALUES ( %s, %s, %s, %s)",
-                       (username, 'Tokyo', email_address,'Japan',password))
-        mysql.connection.commit()
-        msg = 'You have successfully registered!'
-
+#cursor.execute("INSERT INTO user (user_id,username,city,email_address,country,password) VALUES ( %s, %s, %s, %s)",(username, 'Tokyo', email_address,'Japan',password))
+# cursor.execute('SELECT * FROM user WHERE username = %s', (username,))
 
 
 
